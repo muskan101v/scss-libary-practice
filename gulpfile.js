79feaf377c224aa83eeeef98f705b14f24a6d1d9
Muskan Varshney <varshney.muskan1998@gmail.com>
2022-10-08 13:25:15 +0530

    add images and explore new component

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
 
 function buildStyles() {
-  return src("home/**/*.scss").pipe(sass()).pipe(dest("css"));
+  return src("home/**/*.scss")
+    .pipe(sass())
+    .pipe(purgecss({ content: [] }))
+    .pipe(dest("css"));
 }
 
 function watchTask() {
